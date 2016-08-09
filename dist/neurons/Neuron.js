576a09b9ec7c5bcad19ad834d6f1cfde4ad95e52
TimboKZ <tima_kuzh@inbox.ru>
2016-08-09 21:41:43 +0100

    Remove ReLU

--- dist/neurons/Neuron.js
+++ dist/neurons/Neuron.js
@@ -5,7 +5,7 @@
  * @author Timur Kuzhagaliyev <tim@xaerus.co.uk>
  * @copyright 2016
  * @license https://opensource.org/licenses/mit-license.php MIT License
- * @version 0.0.7
+ * @version 0.0.8
  */
 /**
  * Class representing the base neuron, used in hidden layers
@@ -26,7 +26,8 @@ var Neuron = (function () {
      * Logic for the forward pass, similar to:
      * output = ax + by + ... + cz + d
      * Where a,b,...c,d are variable units stored in the neuron and x,y,...z are values of input units
-     * @since 0.0.6 Add ReLU
+     * @since 0.0.8 Removed ReLU
+     * @since 0.0.6 Added ReLU
      * @since 0.0.4 Fixed a bug where `i` would be compared to Unit[]
      * @since 0.0.1
      */
@@ -39,12 +40,12 @@ var Neuron = (function () {
             }
             output += coefficient * this.variableUnits[i].value;
         }
-        output = Math.max(0, output);
         this.outputUnit.value = output;
     };
     /**
      * Logic for the backward pass, first backdrops the gradients to the input units and then adjusts the stored
      * variable units using the step size
+     * @since 0.0.8 Removed ReLU
      * @since 0.0.6 Minor tweaks to logic
      * @since 0.0.5 Added a rectifier for inputUnit.gradient
      * @since 0.0.4 Fixed a bug where `i` would be compared to Unit[]
@@ -58,17 +59,8 @@ var Neuron = (function () {
             if (this.inputUnits[i]) {
                 var inputUnit = this.inputUnits[i];
                 coefficient = inputUnit.value;
-                inputUnit.gradient = coefficient >= 0 ? variableUnit.value * this.outputUnit.gradient : 0.0;
             }
-            if (coefficient >= 0) {
-                variableUnit.gradient = coefficient * this.outputUnit.gradient;
-                if (this.inputUnits[i]) {
-                    variableUnit.gradient -= variableUnit.value;
-                }
-            }
-            else {
-                variableUnit.gradient = 0.0;
-            }
+            variableUnit.gradient = coefficient * this.outputUnit.gradient;
             if (stepSize) {
                 variableUnit.value += stepSize * variableUnit.gradient;
             }
