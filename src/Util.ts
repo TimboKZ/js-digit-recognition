e219dee4d7da9e4bd71ce48881c85c0d2ed63701
TimboKZ <tima_kuzh@inbox.ru>
2016-08-10 19:02:10 +0100

    Add logTest() method to Util class

--- src/Util.ts
+++ src/Util.ts
@@ -4,7 +4,7 @@
  * @author Timur Kuzhagaliyev <tim@xaerus.co.uk>
  * @copyright 2016
  * @license https://opensource.org/licenses/mit-license.php MIT License
- * @version 0.0.1
+ * @version 0.0.2
  */
 
 /**
@@ -18,5 +18,21 @@ export class Util {
      */
     public static sigma(x: number): number {
         return 1 / (1 + Math.exp(-x));
+    }
+
+    /**
+     * Logs NN test output
+     * @since 0.0.2
+     */
+    public static logTest(output: string | string[] = '') {
+        let colors = require('colors');
+        let prefix = colors.yellow('[TEST] ');
+        if (typeof output === 'string') {
+            console.log(prefix + output);
+        } else {
+            output.forEach(function(outputString) {
+                console.log(prefix + outputString);
+            });
+        }
     }
 }
