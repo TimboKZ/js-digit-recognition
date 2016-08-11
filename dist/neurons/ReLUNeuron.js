eda2c182cee7686e832f42bcdbd5c97a00590fde
TimboKZ <tima_kuzh@inbox.ru>
2016-08-11 14:24:34 +0100

    Adjust ReLUNeuron to extend LinearNeuron

--- dist/neurons/ReLUNeuron.js
+++ dist/neurons/ReLUNeuron.js
@@ -4,17 +4,18 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
-var Neuron_1 = require('./Neuron');
+var LinearNeuron_1 = require('./LinearNeuron');
 /**
  * File containing all classes and interface related to ReLU neurons
  *
  * @author Timur Kuzhagaliyev <tim@xaerus.co.uk>
  * @copyright 2016
  * @license https://opensource.org/licenses/mit-license.php MIT License
- * @version 0.0.1
+ * @version 0.0.2
  */
 /**
  * ReLU neuron class
+ * @since 0.0.2 Now extends LinearNeuron instead of Neuron which is now abstract
  * @since 0.0.1
  */
 var ReLUNeuron = (function (_super) {
@@ -24,23 +25,25 @@ var ReLUNeuron = (function (_super) {
     }
     /**
      * Applies ReLU after the forward pass of Neuron class
+     * @since 0.0.2 Renamed `forward()` to `forwardLogic()` as per new architecture
      * @since 0.0.1
      */
-    ReLUNeuron.prototype.forward = function () {
-        _super.prototype.forward.call(this);
+    ReLUNeuron.prototype.forwardLogic = function () {
+        _super.prototype.forwardLogic.call(this);
         this.outputUnit.value = Math.max(0, this.outputUnit.value);
     };
     /**
      * Stops the gradient backpropogation if the neuron did not fire
+     * @since 0.0.2 Renamed `backward()` to `backwardLogic()` as per new architecture
      * @since 0.0.1
      */
-    ReLUNeuron.prototype.backward = function (stepSize) {
+    ReLUNeuron.prototype.backwardLogic = function (stepSize) {
         if (this.outputUnit.value === 0.0) {
-            return;
+            this.outputUnit.gradient = 0.0;
         }
-        _super.prototype.backward.call(this, stepSize);
+        _super.prototype.backwardLogic.call(this, stepSize);
     };
     return ReLUNeuron;
-}(Neuron_1.Neuron));
+}(LinearNeuron_1.LinearNeuron));
 exports.ReLUNeuron = ReLUNeuron;
 //# sourceMappingURL=ReLUNeuron.js.map
--- src/neurons/ReLUNeuron.ts
+++ src/neurons/ReLUNeuron.ts
@@ -1,35 +1,38 @@
-import {Neuron} from './Neuron';
+import {LinearNeuron} from './LinearNeuron';
 /**
  * File containing all classes and interface related to ReLU neurons
  *
  * @author Timur Kuzhagaliyev <tim@xaerus.co.uk>
  * @copyright 2016
  * @license https://opensource.org/licenses/mit-license.php MIT License
- * @version 0.0.1
+ * @version 0.0.2
  */
 
 /**
  * ReLU neuron class
+ * @since 0.0.2 Now extends LinearNeuron instead of Neuron which is now abstract
  * @since 0.0.1
  */
-export class ReLUNeuron extends Neuron {
+export class ReLUNeuron extends LinearNeuron {
     /**
      * Applies ReLU after the forward pass of Neuron class
+     * @since 0.0.2 Renamed `forward()` to `forwardLogic()` as per new architecture
      * @since 0.0.1
      */
-    public forward() {
-        super.forward();
+    public forwardLogic() {
+        super.forwardLogic();
         this.outputUnit.value = Math.max(0, this.outputUnit.value);
     }
 
     /**
      * Stops the gradient backpropogation if the neuron did not fire
+     * @since 0.0.2 Renamed `backward()` to `backwardLogic()` as per new architecture
      * @since 0.0.1
      */
-    public backward(stepSize?: number) {
+    public backwardLogic(stepSize?: number) {
         if (this.outputUnit.value === 0.0) {
-            return;
+            this.outputUnit.gradient = 0.0;
         }
-        super.backward(stepSize);
+        super.backwardLogic(stepSize);
     }
 }
