bd0c0c8ec12a4863799f7b163917c733c5d59b7a
TimboKZ <tima_kuzh@inbox.ru>
2016-08-12 09:42:37 +0100

    Update digit classifier neuron layers

--- dist/test/digit-classification-test.js
+++ dist/test/digit-classification-test.js
@@ -1,6 +1,7 @@
 "use strict";
 var DataParser_1 = require('../src/DataParser');
 var DigitClassifier_1 = require('../src/DigitClassifier');
+var Util_1 = require('../src/Util');
 var LinearNeuron_1 = require('../src/neurons/LinearNeuron');
 var ReLUNeuron_1 = require('../src/neurons/ReLUNeuron');
 /**
@@ -9,7 +10,7 @@ var ReLUNeuron_1 = require('../src/neurons/ReLUNeuron');
  * @author Timur Kuzhagaliyev <tim@xaerus.co.uk>
  * @copyright 2016
  * @license https://opensource.org/licenses/mit-license.php MIT License
- * @version 0.0.4
+ * @version 0.0.5
  */
 // PREPARE THE DATA SETS
 var dataSets = [];
@@ -25,22 +26,17 @@ dataSets.push(DataParser_1.DataParser.getDataSet(8));
 dataSets.push(DataParser_1.DataParser.getDataSet(9));
 var dataSet = DataParser_1.DataParser.combineDataSets(dataSets, true);
 // INITIALISE A DIGIT CLASSIFIER
-var coefficientGenerator = function () { return (Math.random() - 0.5); };
+var coefficientGenerator = function () { return Util_1.Util.randomInRage(-0.5, 0.5); };
 var inputUnitsCount = DataParser_1.IMAGE_SIZE * DataParser_1.IMAGE_SIZE;
 var hiddenLayers = [
     {
         coefficientGenerator: coefficientGenerator,
-        neuronCount: 64,
+        neuronCount: 32,
         neuronType: LinearNeuron_1.LinearNeuron,
     },
     {
         coefficientGenerator: coefficientGenerator,
-        neuronCount: 64,
-        neuronType: ReLUNeuron_1.ReLUNeuron,
-    },
-    {
-        coefficientGenerator: coefficientGenerator,
-        neuronCount: 16,
+        neuronCount: 512,
         neuronType: ReLUNeuron_1.ReLUNeuron,
     },
     {
@@ -53,15 +49,40 @@ var digitClassifier = new DigitClassifier_1.DigitClassifier(inputUnitsCount, hid
 // SETUP THE TESTING/TRAINING ROUTINE
 var testingMatrices = dataSet.testingSet.slice(0, 1000);
 var trainingMatrices = dataSet.trainingSet.slice(0, 11000);
-var trainIterations = 5;
-var trainRounds = 100;
+var trainIterations = 1;
+var trainRounds = 250;
 var colors = require('colors/safe');
 console.time(colors.green('Time elapsed'));
 console.log();
 for (var i = 0; i < trainRounds; i++) {
     digitClassifier.test([testingMatrices[Math.floor(Math.random() * testingMatrices.length)]], true);
     var accuracy = digitClassifier.test(testingMatrices);
-    console.log('Accuracy after ' + i * trainIterations + ' iterations: ' + accuracy.toFixed(3));
-    digitClassifier.train(trainingMatrices, 0.0001, trainIterations);
+    var displayIterations = (i * trainIterations).toString();
+    while (displayIterations.length < 3) {
+        displayIterations = ' ' + displayIterations;
+    }
+    displayIterations = colors.yellow(displayIterations);
+    var displayAccuracy = colors.yellow(accuracy.toFixed(3));
+    var line = '[';
+    for (var i_1 = 0; i_1 < accuracy * 20; i_1++) {
+        line += '-';
+    }
+    line += '>';
+    line = colors.green(line);
+    console.log('Accuracy after ' + displayIterations + ' iterations: ' + displayAccuracy + ' ' + line);
+    var stepSize = 0.0001;
+    if (accuracy > 0.3) {
+        stepSize = 0.00001;
+    }
+    if (accuracy > 0.50) {
+        stepSize = 0.000001;
+    }
+    if (accuracy > 0.65) {
+        stepSize = 0.0000008;
+    }
+    if (accuracy > 0.75) {
+        stepSize = 0.0000006;
+    }
+    digitClassifier.train(trainingMatrices, stepSize, trainIterations);
 }
 console.timeEnd(colors.green('Time elapsed'));
--- test/digit-classification-test.ts
+++ test/digit-classification-test.ts
@@ -1,7 +1,7 @@
 import {DataParser, IDigitDataSet, IDigitMatrix, IMAGE_SIZE} from '../src/DataParser';
 import {DigitClassifier} from '../src/DigitClassifier';
-import {Util} from '../src/Util';
 import {ILayerConfiguration} from '../src/Layer';
+import {Util} from '../src/Util';
 import {LinearNeuron} from '../src/neurons/LinearNeuron';
 import {ReLUNeuron} from '../src/neurons/ReLUNeuron';
 /**
@@ -10,7 +10,7 @@ import {ReLUNeuron} from '../src/neurons/ReLUNeuron';
  * @author Timur Kuzhagaliyev <tim@xaerus.co.uk>
  * @copyright 2016
  * @license https://opensource.org/licenses/mit-license.php MIT License
- * @version 0.0.4
+ * @version 0.0.5
  */
 
 // PREPARE THE DATA SETS
@@ -28,22 +28,17 @@ dataSets.push(DataParser.getDataSet(9));
 let dataSet = DataParser.combineDataSets(dataSets, true);
 
 // INITIALISE A DIGIT CLASSIFIER
-let coefficientGenerator = () => (Math.random() - 0.5);
+let coefficientGenerator = () => Util.randomInRage(-0.5, 0.5);
 let inputUnitsCount = IMAGE_SIZE * IMAGE_SIZE;
 let hiddenLayers: ILayerConfiguration[] = [
     {
         coefficientGenerator: coefficientGenerator,
-        neuronCount: 64,
+        neuronCount: 32,
         neuronType: LinearNeuron,
     },
     {
         coefficientGenerator: coefficientGenerator,
-        neuronCount: 64,
-        neuronType: ReLUNeuron,
-    },
-    {
-        coefficientGenerator: coefficientGenerator,
-        neuronCount: 16,
+        neuronCount: 512,
         neuronType: ReLUNeuron,
     },
     {
@@ -57,15 +52,40 @@ let digitClassifier = new DigitClassifier(inputUnitsCount, hiddenLayers);
 // SETUP THE TESTING/TRAINING ROUTINE
 let testingMatrices: IDigitMatrix[] = dataSet.testingSet.slice(0, 1000);
 let trainingMatrices: IDigitMatrix[] = dataSet.trainingSet.slice(0, 11000);
-let trainIterations = 5;
-let trainRounds = 100;
+let trainIterations = 1;
+let trainRounds = 250;
 let colors = require('colors/safe');
 console.time(colors.green('Time elapsed'));
 console.log();
 for (let i = 0; i < trainRounds; i++) {
     digitClassifier.test([testingMatrices[Math.floor(Math.random() * testingMatrices.length)]], true);
     let accuracy = digitClassifier.test(testingMatrices);
-    console.log('Accuracy after ' + i * trainIterations + ' iterations: ' + accuracy.toFixed(3));
-    digitClassifier.train(trainingMatrices, 0.0001, trainIterations);
+    let displayIterations = (i * trainIterations).toString();
+    while (displayIterations.length < 3) {
+        displayIterations = ' ' + displayIterations;
+    }
+    displayIterations = colors.yellow(displayIterations);
+    let displayAccuracy = colors.yellow(accuracy.toFixed(3));
+    let line = '[';
+    for (let i = 0; i < accuracy * 20; i++) {
+        line += '-';
+    }
+    line += '>';
+    line = colors.green(line);
+    console.log('Accuracy after ' + displayIterations + ' iterations: ' + displayAccuracy + ' ' + line);
+    let stepSize = 0.0001;
+    if (accuracy > 0.3) {
+        stepSize = 0.00001;
+    }
+    if (accuracy > 0.50) {
+        stepSize = 0.000001;
+    }
+    if (accuracy > 0.65) {
+        stepSize = 0.0000008;
+    }
+    if (accuracy > 0.75) {
+        stepSize = 0.0000006;
+    }
+    digitClassifier.train(trainingMatrices, stepSize, trainIterations);
 }
 console.timeEnd(colors.green('Time elapsed'));
